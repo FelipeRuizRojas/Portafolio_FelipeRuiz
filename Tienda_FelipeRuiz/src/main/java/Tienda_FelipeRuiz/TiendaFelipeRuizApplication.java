a7893fdce0f85dbe102a94f532df07f8b03bc0ab
FelipeRuizRojas <feliruizrojas29@gmail.com>
2026-01-24 15:32:21 -0600

    Commit inicial

---
+++ Tienda_FelipeRuiz/src/main/java/Tienda_FelipeRuiz/TiendaFelipeRuizApplication.java
@@ -0,0 +1,13 @@
+package Tienda_FelipeRuiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaFelipeRuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaFelipeRuizApplication.class, args);
+	}
+
+}
