45e5873d304214889a7c2431c2e541bd32701e0e
FelipeRuizRojas <feliruizrojas29@gmail.com>
2026-02-02 10:57:54 -0600

    Commit tienda_FelipeRuizLec3 avance superior

---
+++ src/main/java/Tienda_FelipeRuiz/demo/Projectconfig.java
@@ -0,0 +1,75 @@
+package Tienda_FelipeRuiz.demo;
+
+import java.util.Locale;
+import org.springframework.boot.autoconfigure.web.WebProperties.LocaleResolver;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+
+@Configuration
+public class Projectconfig implements WebMvcConfigurer {
+
+    /* Métodos para configurar las vistas sin controlador */
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/ejemplo2").setViewName("ejemplo2");
+        registry.addViewController("/multimedia").setViewName("multimedia");
+        registry.addViewController("/iframes").setViewName("iframes");
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/registro/nuevo").setViewName("registro/nuevo");
+    }
+
+    /* Resolver de plantillas Thymeleaf */
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver_0() {
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setPrefix("classpath:/templates/");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode(TemplateMode.HTML);
+        resolver.setOrder(0);
+        resolver.setCheckExistence(true);
+        return resolver;
+    }
+    
+    @Bean
+    public SessionLocaleResolver localeResolver() {
+    var slr = new SessionLocaleResolver();
+    slr.setDefaultLocale(Locale.getDefault());
+    slr.setLocaleAttributeName("session.current.locale");
+    slr.setTimeZoneAttributeName("session.current.timezone");
+    return slr;
+    }
+
+    @Bean
+public LocaleChangeInterceptor localeChangeInterceptor() {
+    var lci = new LocaleChangeInterceptor();
+    lci.setParamName("lang");
+    return lci;
+}
+
+@Override
+public void addInterceptors(InterceptorRegistry registro) {
+    registro.addInterceptor(localeChangeInterceptor());
+}
+
+
+@Bean("messageSource")
+public MessageSource messageSource() {
+    ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+    messageSource.setBasenames("messages");
+    messageSource.setDefaultEncoding("UTF-8");
+    return messageSource;
+}
+}
---
+++ src/test/java/Tienda_FelipeRuiz/TiendaFelipeRuizApplicationTests.java
@@ -0,0 +1,13 @@
+package Tienda_FelipeRuiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaFelipeRuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
